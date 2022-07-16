42353178a6c57634a7dd72f9edc25df7da578d32
crismarmon <cristhianmm@gmail.com>
2022-07-15 21:38:37 -0500

    demasiado mejor

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -203,6 +203,9 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var str = str + "!"
+  return str
+
 }
 
 function combinarNombres(nombre, apellido) {
